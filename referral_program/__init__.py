ce8c68f96048d7ddce77bbf198bbd1b753c0c375
Renier Venter <renier.venter@typedev.co.za>
2020-02-09 12:57:18 +0200

    Modified init to load db url from env variable is set

--- referral_program/__init__.py
+++ referral_program/__init__.py
@@ -1,8 +1,12 @@
 from pyramid.config import Configurator
+import os
 
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application."""
+    db_url = os.environ.get('SQLALCHEMY_URL')
+    if db_url:
+        settings['sqlalchemy.url'] = db_url
     config = Configurator(settings=settings)
     config.include('.models')
     config.include('.routes')
